5132b4c8d29599d5279456e4092a5da57e5d9ad3
Leagian <lea.giangrosso@gmail.com>
2024-01-05 13:52:48 +0100

    Updated part2 countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,74 +1,55 @@
-import { useState } from "react";
-
-const Buttons = ({ handleClick, text }) => {
-  return (
-    <div className="buttons">
-      <button onClick={handleClick}>{text}</button>
-    </div>
-  );
-};
-
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  );
-};
-
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad;
-  const average = (good - bad) / total;
-  const positive = (good / total) * 100;
-
-  if (total === 0) {
-    return <p>No feedback given</p>;
-  }
-
-  return (
-    <div className="stats">
-      <h1>Statistics</h1>
-      <table>
-        <tbody>
-          <StatisticLine text="Good:" value={good} />
-          <StatisticLine text="Neutral:" value={neutral} />
-          <StatisticLine text="Bad:" value={bad} />
-          <StatisticLine text="All:" value={total} />
-          <StatisticLine text="Average:" value={average.toFixed(2)} />
-          <StatisticLine text="Positive:" value={`${positive.toFixed(2)}%`} />
-        </tbody>
-      </table>
-    </div>
-  );
-};
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState('');
 
-  const addGood = () => {
-    setGood(good + 1);
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleCountryChange = (event) => {
+    setSearch(event.target.value);
   };
 
-  const addNeutral = () => {
-    setNeutral(neutral + 1);
-  };
-
-  const addBad = () => {
-    setBad(bad + 1);
-  };
+  const countriesToShow =
+    search === ''
+      ? countries
+      : countries.filter((country) =>
+          country.name.common.toLowerCase().includes(search.toLowerCase())
+        );
 
   return (
-    <div>
-      <h1>Give feeback</h1>
-      <Buttons handleClick={addGood} text="Good" />
-      <Buttons handleClick={addNeutral} text="Neutral" />
-      <Buttons handleClick={addBad} text="Bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
+    <>
+      <label>
+        find countries <input onChange={handleCountryChange} value={search} />
+      </label>
+      {countriesToShow.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : (
+        countriesToShow.map((country) =>
+          countriesToShow.length === 1 ? (
+            <div key={country.cca3}>
+              <h1>{country.name.common}</h1>
+              <p>Capital {country.capital}</p>
+              <p>Area {country.area}</p>
+              <h3>Languages</h3>
+              <ul>
+                {Object.values(country.languages).map((language) => (
+                  <li key={country.cca3}>{language}</li>
+                ))}
+              </ul>
+              <img src={country.flags.png} alt={country.flags.alt} />
+            </div>
+          ) : (
+            <p key={country.cca3}>{country.name.common}</p>
+          )
+        )
+      )}
+    </>
   );
 };
-
 export default App;
