128a9d920ad78991fe4222b92299e9c7d98ddefa
Leagian <lea.giangrosso@gmail.com>
2024-01-05 09:46:48 +0100

    Updated part2 courseinfo

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -3,44 +3,44 @@ import Course from "./Course";
 const App = () => {
   const courses = [
     {
-      name: "Half Stack application development",
       id: 1,
+      name: "Half Stack application development",
       parts: [
         {
+          id: 1,
           name: "Fundamentals of React",
           exercises: 10,
-          id: 1,
         },
         {
+          id: 2,
           name: "Using props to pass data",
           exercises: 7,
-          id: 2,
         },
         {
+          id: 3,
           name: "State of a component",
           exercises: 14,
-          id: 3,
         },
         {
+          id: 4,
           name: "Redux",
           exercises: 11,
-          id: 4,
         },
       ],
     },
     {
-      name: "Node.js",
       id: 2,
+      name: "Node.js",
       parts: [
         {
+          id: 1,
           name: "Routing",
           exercises: 3,
-          id: 1,
         },
         {
+          id: 2,
           name: "Middlewares",
           exercises: 7,
-          id: 2,
         },
       ],
     },
