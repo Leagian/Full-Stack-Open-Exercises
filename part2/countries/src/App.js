ab39737b8342cecb58cf3f39e5b6a0a3df78fe79
Leagian <lea.giangrosso@gmail.com>
2024-01-06 09:48:46 +0100

    2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,6 +5,8 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
   const [showIndex, setShowIndex] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const api_key = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((response) => {
@@ -23,6 +25,18 @@ const App = () => {
           country.name.common.toLowerCase().includes(search.toLowerCase())
         );
 
+  useEffect(() => {
+    if (countriesToShow.length === 1) {
+      axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/weather?q=${countriesToShow[0].capital}&appid=${api_key}&units=metric`
+        )
+        .then((response) => {
+          setWeather(response.data);
+        });
+    }
+  }, [countriesToShow, api_key]);
+
   const handleShow = (index) => {
     setShowIndex(showIndex === index ? null : index);
   };
@@ -39,11 +53,13 @@ const App = () => {
           <div key={country.cca3}>
             <p>
               {country.name.common}{' '}
-              <button onClick={() => handleShow(index)}>
-                {showIndex === index ? 'Hide' : 'Show'}
-              </button>
+              {countriesToShow.length > 1 && (
+                <button onClick={() => handleShow(index)}>
+                  {showIndex === index ? 'Hide' : 'Show'}
+                </button>
+              )}
             </p>
-            {showIndex === index && (
+            {(showIndex === index || countriesToShow.length === 1) && (
               <>
                 <h1>{country.name.common}</h1>
                 <p>Capital {country.capital}</p>
@@ -57,6 +73,17 @@ const App = () => {
                   )}
                 </ul>
                 <img src={country.flags.png} alt={country.flags.alt} />
+                {countriesToShow.length === 1 && weather && (
+                  <>
+                    <h1>Weather in {weather.name}</h1>
+                    <p>Temperature {weather.main.temp}</p>
+                    <img
+                      src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                      alt={weather.weather[0].description}
+                    />
+                    <p>Wind {weather.wind.speed} m/s</p>
+                  </>
+                )}
               </>
             )}
           </div>
