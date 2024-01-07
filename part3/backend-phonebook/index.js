c43c4c63084a2fc77fd0b708982e495d79c21c44
Leagian <lea.giangrosso@gmail.com>
2024-01-07 12:52:47 +0100

    3.1

---
+++ part3/backend-phonebook/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.send(`This id ${id} doesn't exist`);
+    res.status(400).end();
+  }
+});
+
+//*? Generate Id Function ?*//
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  if (!body.name) {
+    return res.status(400).json({ error: 'name missing' });
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
