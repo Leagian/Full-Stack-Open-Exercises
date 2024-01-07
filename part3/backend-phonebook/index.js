68edc5f51588e893ed46b3b7cab07160b9d9d416
Leagian <lea.giangrosso@gmail.com>
2024-01-07 13:03:55 +0100

    3.2

--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -30,6 +30,12 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
 
+app.get('/info', (req, res) => {
+  const date = new Date();
+  const count = persons.length;
+  res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
+});
+
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
