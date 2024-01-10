d8c350d6d0d18ba85467ecc343a9632171433279
Leagian <lea.giangrosso@gmail.com>
2024-01-10 19:02:48 +0100

    setup heroku

--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -93,7 +93,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
