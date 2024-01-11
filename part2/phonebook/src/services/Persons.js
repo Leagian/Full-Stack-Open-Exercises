f5b4f80d092eedf8c031a615d8c22138a0106161
Leagian <lea.giangrosso@gmail.com>
2024-01-11 18:50:20 +0100

    Changed frontend routes for heroku and added Build folder

--- part2/phonebook/src/services/Persons.js
+++ part2/phonebook/src/services/Persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl =
+  'https://peaceful-shelf-14414-c1b424c5f661.herokuapp.com/api/persons';
 
 const getAllPersons = () => {
   const request = axios.get(baseUrl);
--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -93,7 +93,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = process.env.PORT || PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
