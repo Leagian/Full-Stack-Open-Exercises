a3dd1cccf2054c44c199a427ddb44a62c93ac70a
Leagian <lea.giangrosso@gmail.com>
2024-01-14 12:00:26 +0100

    3.12

--- part3/backend-phonebook/mongo.js
+++ part3/backend-phonebook/mongo.js
@@ -13,7 +13,7 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.gey3fqx.mongodb.net/ph
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: Number,
+  number: String,
 });
 
 const Person = mongoose.model("Person", personSchema);
@@ -21,17 +21,23 @@ const Person = mongoose.model("Person", personSchema);
 mongoose.connect(url).then((result) => {
   console.log("connected");
 
-  //     const person = new Person({
-  //       name: "Cooks",
-  //       number: 123456789,
-  //     });
-
-  //     return person.save();
-  //   })
-  Person.find({}).then((result) => {
-    result.forEach((person) => {
-      console.log(person);
+  if (process.argv.length === 5) {
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4],
     });
-    mongoose.connection.close();
-  });
+
+    person.save().then(() => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`);
+      mongoose.connection.close();
+    });
+  } else if (process.argv.length === 3) {
+    console.log("phonebook:");
+    Person.find({}).then((result) => {
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.number}`);
+      });
+      mongoose.connection.close();
+    });
+  }
 });
